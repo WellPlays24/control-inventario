ca07afcc1d1dfc158dd8fd5a2e2e47b5442b0d32
WellPlays24 <wellington.castillocisf@gmail.com>
2025-07-26 15:00:03 -0500

    Configura rutas y añade router-outlet con navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { TransaccionesComponent } from './pages/transacciones/transacciones.component';
+import { NuevaTransaccionComponent } from './pages/nueva-transaccion/nueva-transaccion.component';
+
+export const routes: Routes = [
+
+    {path: '', component: InicioComponent},
+    {path: 'transacciones', component: TransaccionesComponent},
+    {path: 'nueva-transaccion', component: NuevaTransaccionComponent},
+
+
+];
